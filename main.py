fa883ba5107a4b8cd2990f77caf2351b87d62981
MaxRo89 <rmax9910@gmail.com>
2025-08-25 22:32:42 -0600

    avances

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+import constantes
+import personaje
+import world
+#iniciliazamos pygame
+pygame.init()
+
+
+WIDTH, HEIGHT = 800,600 #La medida de nuestro juego
+ventana = pygame.display.set_mode((constantes.WIDTH, constantes.HEIGHT))
+pygame.display.set_caption("Simulador de vida salvaje") #Nombre de la ventana del juego
+
+def main():
+    clock = pygame.time.Clock() #para que se vaya actualizando el juego
+    world = World(constantes.WIDTH, constantes.HEIGHT)
+    personaje = Personaje(constantes.WIDTH // 2, constantes.HEIGHT//2)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #si apretamos la x de nuestra ventana se sale
+                pygame.quit()
+                sys.exit()
+                
+if __name__ == "__main__":
+    main()
+            
